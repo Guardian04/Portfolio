9226c3b5c8926353009311e541e58a05363d8e97
Guardian04 <guardia.guardian04@gmail.com>
2023-08-31 14:08:44 +0200

    update

--- src/app/desktop/components/menu/MenuFooter.tsx
+++ src/app/desktop/components/menu/MenuFooter.tsx
@@ -32,15 +32,21 @@ export default function MenuFooter({ openMenu, onClickHamburger, currentLanguage
 
     const onClickLanguage = (lang: string) => {
         onClickLang(lang);
-        onClickHamburger();
+        if (lang !== languages[0] && lang!== languages[3]) {
+            onClickHamburger();
+        }
     };
 
     return (
         <div className="menu-footer">
+            <div className={`btn-more-lang ${openMenu ? "sub" : ""}`} onClick={() => onClickHamburger()}>
+                <span></span>
+                <span></span>
+            </div>
             <div className="menu-footer-container">
                 {languages.map((lang) => {
                     return (
-                        <div key={lang} className={`menu-footer-lang ${lang === currentLanguage ? "selected" : ""}`} id={lang} onClick={() => onClickLanguage(lang)}>
+                        <div key={lang} className={`menu-footer-lang ${lang} ${lang === currentLanguage ? "selected" : ""}`} onClick={() => onClickLanguage(lang)}>
                             <span>{lang.toUpperCase()}</span>
                         </div>
                     );
