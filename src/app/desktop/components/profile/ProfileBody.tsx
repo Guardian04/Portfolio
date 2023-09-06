0a5dbe708eecd1475e1cc455dd6eb14edf837d71
Guardian04 <guardia.guardian04@gmail.com>
2023-09-06 13:52:57 +0200

    update

--- src/app/desktop/components/profile/ProfileBody.tsx
+++ src/app/desktop/components/profile/ProfileBody.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import "./ProfileBody.css";
 
@@ -65,38 +66,69 @@ export default function ProfileBody() {
                 </div>
                 <div className="profile-body-speak">
                     {languages.map((lang) => {
-                        const radius = 30;
+                        const radius = 25;
                         const circumference = 2 * Math.PI * radius;
                         const progress = ((100 - lang.Percentage) / 100) * circumference;
+
+                        const [offset, setOffset] = useState(circumference);
+                        const easeOutCubic = (t: number) => 1 - Math.pow(1 - t, 3);
+
+                        useEffect(() => {
+                        const animationDuration = 3000;
+
+                        const animateCircle = () => {
+                            let start = Date.now();
+                            const end = start + animationDuration;
+
+                            function step() {
+                            const now = Date.now();
+                            const percentage = Math.min(1, (now - start) / animationDuration);
+                            const easedPercentage = easeOutCubic(percentage); // Appliquer une fonction d'interpolation (easeOutCubic dans cet exemple)
+                            const currentOffset = circumference - easedPercentage * (circumference - progress);
+                            setOffset(currentOffset);
+
+                            if (now < end) {
+                                requestAnimationFrame(step);
+                            }
+                            }
+
+                            requestAnimationFrame(step);
+                        };
+
+                        animateCircle();
+                        }, [circumference, progress]);
+
                         return (
                             <div key={lang.Name} className="profile-body-speak-item">
-                                <svg width="80" height="80">
+                                <svg width="70" height="70">
                                     <circle
-                                        cx="40"
-                                        cy="40"
+                                        cx="35"
+                                        cy="35"
                                         r={radius}
                                         fill="transparent"
                                         stroke="#10222E"
                                         strokeWidth="4"
                                     />
                                     <circle
-                                        cx="40"
-                                        cy="40"
+                                        cx="35"
+                                        cy="35"
                                         r={radius}
                                         fill="transparent"
                                         stroke="#FFA500"
                                         strokeWidth="4"
                                         strokeDasharray={circumference}
-                                        strokeDashoffset={progress}
+                                        strokeDashoffset={offset}
                                         strokeLinecap="round"
-                                        transform={`rotate(-90 40 40)`}
-                                    />
+                                        transform={`rotate(-90 35 35)`}
+                                    >
+                                    </circle>
                                     <text 
                                         x="50%" 
                                         y="50%" 
                                         textAnchor="middle" 
                                         dominantBaseline="middle" 
                                         fill="white"
+                                        fontSize="0.8rem"
                                     >
                                         {lang.Level}
                                     </text>
